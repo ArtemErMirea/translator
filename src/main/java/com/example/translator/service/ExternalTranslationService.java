9e4cc1115144aa1d452b02b9f96b56318f322d01
ArtemErMirea <artem.e.2001@gmail.com>
2024-08-01 21:26:26 +0300

    Эксперименты с подключением к API

--- src/main/java/com/example/translator/service/ExternalTranslationService.java
+++ src/main/java/com/example/translator/service/ExternalTranslationService.java
@@ -1,22 +1,54 @@
 package com.example.translator.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponentsBuilder;
 
-//Сервис для взаимодействия с внешним API перевода
+import java.util.Map;
+import java.util.List;
+
 @Service
 public class ExternalTranslationService implements TranslationAPI {
 
     private final RestTemplate restTemplate;
 
+    @Value("${rapidapi.key}")
+    private String rapidApiKey;
+
     public ExternalTranslationService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
+    @Override
     public String translateWord(String word, String sourceLang, String targetLang) {
-        // URL API перевода
-        String url = "http://translate.google.ru/translate_a/t?client=x&text="+ word +"&hl=" + sourceLang + "&sl=en&tl=" + targetLang;
-        //String url = "https://api.externaltranslation.com/translate?word=" + word + "&source=" + sourceLang + "&target=" + targetLang;
-        return restTemplate.getForObject(url, String.class);
+        String url = UriComponentsBuilder.fromHttpUrl("https://google-translate1.p.rapidapi.com/language/translate/v2")
+                .queryParam("q", word)
+                .queryParam("source", sourceLang)
+                .queryParam("target", targetLang)
+                .queryParam("format", "text")
+                .toUriString();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/x-www-form-urlencoded");
+        headers.set("X-RapidAPI-Host", "google-translate1.p.rapidapi.com");
+        headers.set("X-RapidAPI-Key", rapidApiKey);
+
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<Map> response = restTemplate.exchange(url, HttpMethod.POST, entity, Map.class);
+
+        Map<String, Object> data = (Map<String, Object>) response.getBody().get("data");
+        if (data != null) {
+            Map<String, Object> translations = (Map<String, Object>) data.get("translations");
+            if (translations != null && !translations.isEmpty()) {
+                return (String) translations.get("translatedText");
+            }
+        }
+        return null;
     }
 }
